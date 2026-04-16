051ca10f183a0801628a944c0c520eddfb172a12
arpit28-hub <140876581+arpit28-hub@users.noreply.github.com>
2026-04-16 16:36:13 +0530

    commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -36,7 +36,7 @@ const Footer = () => {
       <div>
         <hr />
         <p className="py-5 text-sm text-center ">
-          {/* Copyright 2024@ forever.com- All Right Reserved */}
+          Copyright 2024@ forever.com- All Right Reserved
         </p>
       </div>
     </div>
